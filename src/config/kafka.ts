d91a73b2037e9d61bbdaa25b416c82ca6a1d2408
mo14337 <mohitsingh14336@gmail.com>
2025-03-28 21:29:33 +0530

    Add kafka broker methods

---
+++ src/config/kafka.ts
@@ -0,0 +1,37 @@
+import { Kafka, Producer } from "kafkajs";
+import { MessageProducerBroker } from "../common/types/broker";
+
+export class KafkaProducerBroker implements MessageProducerBroker {
+    private producer: Producer;
+
+    constructor(clientId: string, brokers: string[]) {
+        const kafka = new Kafka({
+            clientId: clientId,
+            brokers: brokers,
+        });
+        this.producer = kafka.producer();
+    }
+
+    async connect() {
+        await this.producer.connect();
+    }
+
+    async disconnect() {
+        if (this.producer) {
+            return await this.producer.disconnect();
+        }
+    }
+    /**
+     *
+     * @param topic - this is the topic to which the message will be sent
+     * @param message - this is the message that will be sent to the topic
+     * @throws {Error} - if the message is not sent successfully
+     */
+
+    async sendMessages(topic: string, message: string) {
+        await this.producer.send({
+            topic: topic,
+            messages: [{ value: message }],
+        });
+    }
+}
