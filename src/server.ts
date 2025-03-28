f882f7026183282a327664fd38324437fdd4e5fb
mo14337 <mohitsingh14336@gmail.com>
2025-03-28 21:36:35 +0530

    Add kafka connection

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,24 @@
 import app from "./app";
+import { MessageProducerBroker } from "./common/types/broker";
 import { initDb } from "./config/db";
+import { KafkaProducerBroker } from "./config/kafka";
 import logger from "./config/logger";
 import config from "config";
 
 const startServer = async () => {
     const PORT: number = config.get("server.port") || 5502;
+    let messageProducerBroker: MessageProducerBroker | null = null;
     try {
         await initDb();
+        messageProducerBroker = new KafkaProducerBroker("catalogue-service", [
+            config.get("kafka.brokers"),
+        ]);
+        await messageProducerBroker.connect();
         app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
     } catch (err: unknown) {
+        if (messageProducerBroker) {
+            await messageProducerBroker.disconnect();
+        }
         if (err instanceof Error) {
             logger.error(err.message);
             logger.on("finish", () => {
