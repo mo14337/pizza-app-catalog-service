506a6877b94b25265bf773b98d7fc7447e77c5e0
mo14337 <mohitsingh14336@gmail.com>
2025-04-23 18:54:08 +0530

    added product and topping event types

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -8,3 +8,14 @@ export interface AuthRequest extends Request {
         tenant?: string;
     };
 }
+
+export enum ProductEvents {
+    PRODUCT_CREATE = "PRODUCT_CREATE",
+    PRODUCT_UPDATE = "PRODUCT_UPDATE",
+    PRODUCT_DELETE = "PRODUCT_DELETE",
+}
+export enum ToppingEvents {
+    TOPPING_CREATE = "TOPPING_CREATE",
+    TOPPING_UPDATE = "TOPPING_UPDATE",
+    TOPPING_DELETE = "TOPPING_DELETE",
+}
